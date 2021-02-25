dfb1ebbbdb37486321fe33a225ae8d5179fa4b43
Esmee Peters <info@esmee-peters.nl>
2021-02-25 09:51:12 +0100

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './compontents/App';
+
+import './style/index.scss';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
